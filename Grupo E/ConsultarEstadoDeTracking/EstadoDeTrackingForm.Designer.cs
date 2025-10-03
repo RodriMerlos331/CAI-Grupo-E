158d9093cdb83aa321aeb98591c2aabb2b43368a
RodriMerlos331 <merlosrodrigo25@gmail.com>
2025-10-03 16:25:35 -0300

    Pull commit

---
+++ Grupo E/ConsultarEstadoDeTracking/EstadoDeTrackingForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Grupo_E.ConsultarEstadoDeTracking
+{
+    partial class EstadoDeTrackingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.IdTrackerText = new System.Windows.Forms.TextBox();
+            this.BotonBuscar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.TrackerIDColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EstadoColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FechaHoraColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.UbicacionColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TransportistaColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.HDRColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.OrigenColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DestinoColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BultoColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ObservacionesColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BotonBuscar);
+            this.groupBox1.Controls.Add(this.IdTrackerText);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(967, 109);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresa ID del Tracker";
+            // 
+            // IdTrackerText
+            // 
+            this.IdTrackerText.Location = new System.Drawing.Point(6, 47);
+            this.IdTrackerText.Name = "IdTrackerText";
+            this.IdTrackerText.Size = new System.Drawing.Size(844, 20);
+            this.IdTrackerText.TabIndex = 0;
+            // 
+            // BotonBuscar
+            // 
+            this.BotonBuscar.Location = new System.Drawing.Point(859, 44);
+            this.BotonBuscar.Name = "BotonBuscar";
+            this.BotonBuscar.Size = new System.Drawing.Size(102, 23);
+            this.BotonBuscar.TabIndex = 1;
+            this.BotonBuscar.Text = "Buscar";
+            this.BotonBuscar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(14, 136);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(964, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Descripcion de estado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estado actual:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "XXXXXXXX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha / Hora:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(278, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "XX/XX/XXXX XX:XX";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(432, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Localidad:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(494, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "XXXXXXXXX";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listView1);
+            this.groupBox3.Location = new System.Drawing.Point(14, 258);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(958, 317);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Historial";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.TrackerIDColumn,
+            this.EstadoColumn,
+            this.FechaHoraColumn,
+            this.UbicacionColumn,
+            this.TransportistaColumn,
+            this.HDRColumn,
+            this.OrigenColumn,
+            this.DestinoColumn,
+            this.BultoColumn,
+            this.ObservacionesColumn});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(9, 19);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(935, 283);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // TrackerIDColumn
+            // 
+            this.TrackerIDColumn.Text = "Tracker ID";
+            this.TrackerIDColumn.Width = 74;
+            // 
+            // EstadoColumn
+            // 
+            this.EstadoColumn.Text = "Estado";
+            // 
+            // FechaHoraColumn
+            // 
+            this.FechaHoraColumn.Text = "Fecha / Hora";
+            // 
+            // UbicacionColumn
+            // 
+            this.UbicacionColumn.Text = "Ubicacion";
+            // 
+            // TransportistaColumn
+            // 
+            this.TransportistaColumn.Text = "Transportista Asignado";
+            // 
+            // HDRColumn
+            // 
+            this.HDRColumn.Text = "Hoja de Ruta";
+            // 
+            // OrigenColumn
+            // 
+            this.OrigenColumn.Text = "Origen";
+            // 
+            // DestinoColumn
+            // 
+            this.DestinoColumn.Text = "Destino";
+            // 
+            // BultoColumn
+            // 
+            this.BultoColumn.Text = "Tipo de Bulto";
+            // 
+            // ObservacionesColumn
+            // 
+            this.ObservacionesColumn.Text = "Observaciones";
+            // 
+            // EstadoDeTrackingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 601);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "EstadoDeTrackingForm";
+            this.Text = "Estado del paquete";
+            this.Load += new System.EventHandler(this.EstadoDeTrackingForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BotonBuscar;
+        private System.Windows.Forms.TextBox IdTrackerText;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader TrackerIDColumn;
+        private System.Windows.Forms.ColumnHeader EstadoColumn;
+        private System.Windows.Forms.ColumnHeader FechaHoraColumn;
+        private System.Windows.Forms.ColumnHeader UbicacionColumn;
+        private System.Windows.Forms.ColumnHeader TransportistaColumn;
+        private System.Windows.Forms.ColumnHeader HDRColumn;
+        private System.Windows.Forms.ColumnHeader OrigenColumn;
+        private System.Windows.Forms.ColumnHeader DestinoColumn;
+        private System.Windows.Forms.ColumnHeader BultoColumn;
+        private System.Windows.Forms.ColumnHeader ObservacionesColumn;
+    }
+}
---
+++ Grupo E/ConsultarEstadoDeTracking/EstadoDeTrackingForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grupo_E.ConsultarEstadoDeTracking
+{
+    public partial class EstadoDeTrackingForm : Form
+    {
+        public EstadoDeTrackingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EstadoDeTrackingForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
