5086daf0cdfb7aa597d43e030e47cc2d4598c992
ConradoIannicelli <conradoiannicelli@gmail.com>
2025-10-04 15:26:33 -0300

    Cambios Conrado2

--- Grupo E/RetirarEnAgencia/RetirarenAgencia.Designer.cs
+++ Grupo E/RetirarEnAgencia/RetirarenAgencia.Designer.cs
@@ -28,12 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNrodeTracking = new System.Windows.Forms.TextBox();
+            this.btnBuscarTracking = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.btnEntregarEncomienda = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero de Tracking";
+            // 
+            // txtNrodeTracking
+            // 
+            this.txtNrodeTracking.Location = new System.Drawing.Point(17, 66);
+            this.txtNrodeTracking.Name = "txtNrodeTracking";
+            this.txtNrodeTracking.Size = new System.Drawing.Size(433, 26);
+            this.txtNrodeTracking.TabIndex = 1;
+            // 
+            // btnBuscarTracking
+            // 
+            this.btnBuscarTracking.Location = new System.Drawing.Point(471, 61);
+            this.btnBuscarTracking.Name = "btnBuscarTracking";
+            this.btnBuscarTracking.Size = new System.Drawing.Size(128, 37);
+            this.btnBuscarTracking.TabIndex = 2;
+            this.btnBuscarTracking.Text = "Buscar";
+            this.btnBuscarTracking.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Datos de retiro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(43, 222);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(407, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.AcceptsReturn = true;
+            this.txtApellido.Location = new System.Drawing.Point(44, 302);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(407, 26);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 354);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "DNI";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.AcceptsReturn = true;
+            this.txtDNI.Location = new System.Drawing.Point(44, 382);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(407, 26);
+            this.txtDNI.TabIndex = 9;
+            // 
+            // btnEntregarEncomienda
+            // 
+            this.btnEntregarEncomienda.Location = new System.Drawing.Point(303, 464);
+            this.btnEntregarEncomienda.Name = "btnEntregarEncomienda";
+            this.btnEntregarEncomienda.Size = new System.Drawing.Size(145, 66);
+            this.btnEntregarEncomienda.TabIndex = 10;
+            this.btnEntregarEncomienda.Text = "Entregar Encomienda";
+            this.btnEntregarEncomienda.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(454, 464);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(145, 66);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // RetirarenAgencia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(611, 542);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnEntregarEncomienda);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscarTracking);
+            this.Controls.Add(this.txtNrodeTracking);
+            this.Controls.Add(this.label1);
+            this.Name = "RetirarenAgencia";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNrodeTracking;
+        private System.Windows.Forms.Button btnBuscarTracking;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.Button btnEntregarEncomienda;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
