911aca241b8e4c0eed47bcfcbbdaba57cc0e65c0
GuillePalencia <101320608+GuillePalencia@users.noreply.github.com>
2025-10-03 21:51:32 -0300

    Add files via upload

---
+++ Grupo E/EstadoCuentasCorrientes/FormEstadoCuentasCorrientes.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace TUTASA.CuentasCorrientes
+{
+    partial class FormEstadoCuentasCorrientes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.grpCliente = new System.Windows.Forms.GroupBox();
+            this.btnBuscarCliente = new System.Windows.Forms.Button();
+            this.txtCUIT = new System.Windows.Forms.TextBox();
+            this.lblCUIT = new System.Windows.Forms.Label();
+            this.grpFiltros = new System.Windows.Forms.GroupBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.txtNroFactura = new System.Windows.Forms.TextBox();
+            this.lblNroFactura = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiarFiltros = new System.Windows.Forms.Button();
+            this.lblEstadoCuentaCorriente = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lvwEstadoCuentaCorriente = new System.Windows.Forms.ListView();
+            this.colPadding = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colNroFactura = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colFecha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colImporte = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colEstado = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colAccion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.grpCliente.SuspendLayout();
+            this.grpFiltros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.Location = new System.Drawing.Point(10, 10);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(862, 27);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Estado de Cuentas Corrientes";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // grpCliente
+            // 
+            this.grpCliente.Controls.Add(this.btnBuscarCliente);
+            this.grpCliente.Controls.Add(this.txtCUIT);
+            this.grpCliente.Controls.Add(this.lblCUIT);
+            this.grpCliente.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpCliente.Location = new System.Drawing.Point(12, 40);
+            this.grpCliente.Name = "grpCliente";
+            this.grpCliente.Size = new System.Drawing.Size(850, 80);
+            this.grpCliente.TabIndex = 1;
+            this.grpCliente.TabStop = false;
+            this.grpCliente.Text = "Cliente";
+            // 
+            // btnBuscarCliente
+            // 
+            this.btnBuscarCliente.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarCliente.Location = new System.Drawing.Point(248, 26);
+            this.btnBuscarCliente.Name = "btnBuscarCliente";
+            this.btnBuscarCliente.Size = new System.Drawing.Size(127, 34);
+            this.btnBuscarCliente.TabIndex = 2;
+            this.btnBuscarCliente.Text = "Buscar Cliente";
+            this.btnBuscarCliente.UseVisualStyleBackColor = true;
+            // 
+            // txtCUIT
+            // 
+            this.txtCUIT.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCUIT.Location = new System.Drawing.Point(76, 30);
+            this.txtCUIT.Name = "txtCUIT";
+            this.txtCUIT.Size = new System.Drawing.Size(150, 30);
+            this.txtCUIT.TabIndex = 1;
+            // 
+            // lblCUIT
+            // 
+            this.lblCUIT.AutoSize = true;
+            this.lblCUIT.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCUIT.Location = new System.Drawing.Point(18, 30);
+            this.lblCUIT.Name = "lblCUIT";
+            this.lblCUIT.Size = new System.Drawing.Size(51, 23);
+            this.lblCUIT.TabIndex = 0;
+            this.lblCUIT.Text = "CUIT:";
+            this.lblCUIT.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // grpFiltros
+            // 
+            this.grpFiltros.Controls.Add(this.dtpFecha);
+            this.grpFiltros.Controls.Add(this.lblFecha);
+            this.grpFiltros.Controls.Add(this.cmbEstado);
+            this.grpFiltros.Controls.Add(this.lblEstado);
+            this.grpFiltros.Controls.Add(this.txtNroFactura);
+            this.grpFiltros.Controls.Add(this.lblNroFactura);
+            this.grpFiltros.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpFiltros.Location = new System.Drawing.Point(14, 136);
+            this.grpFiltros.Name = "grpFiltros";
+            this.grpFiltros.Size = new System.Drawing.Size(850, 100);
+            this.grpFiltros.TabIndex = 2;
+            this.grpFiltros.TabStop = false;
+            this.grpFiltros.Text = "Filtros de búsqueda";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFecha.Location = new System.Drawing.Point(584, 54);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(260, 30);
+            this.dtpFecha.TabIndex = 5;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(580, 30);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(54, 23);
+            this.lblFecha.TabIndex = 4;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(294, 53);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(260, 31);
+            this.cmbEstado.TabIndex = 3;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.Location = new System.Drawing.Point(290, 30);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(61, 23);
+            this.lblEstado.TabIndex = 2;
+            this.lblEstado.Text = "Estado";
+            // 
+            // txtNroFactura
+            // 
+            this.txtNroFactura.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNroFactura.Location = new System.Drawing.Point(6, 53);
+            this.txtNroFactura.Name = "txtNroFactura";
+            this.txtNroFactura.Size = new System.Drawing.Size(260, 30);
+            this.txtNroFactura.TabIndex = 1;
+            // 
+            // lblNroFactura
+            // 
+            this.lblNroFactura.AutoSize = true;
+            this.lblNroFactura.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNroFactura.Location = new System.Drawing.Point(7, 30);
+            this.lblNroFactura.Name = "lblNroFactura";
+            this.lblNroFactura.Size = new System.Drawing.Size(90, 23);
+            this.lblNroFactura.TabIndex = 0;
+            this.lblNroFactura.Text = "Nº Factura";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(24, 242);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(380, 34);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiarFiltros
+            // 
+            this.btnLimpiarFiltros.Location = new System.Drawing.Point(478, 242);
+            this.btnLimpiarFiltros.Name = "btnLimpiarFiltros";
+            this.btnLimpiarFiltros.Size = new System.Drawing.Size(380, 34);
+            this.btnLimpiarFiltros.TabIndex = 4;
+            this.btnLimpiarFiltros.Text = "Limpiar Filtros";
+            this.btnLimpiarFiltros.UseVisualStyleBackColor = true;
+            // 
+            // lblEstadoCuentaCorriente
+            // 
+            this.lblEstadoCuentaCorriente.AutoSize = true;
+            this.lblEstadoCuentaCorriente.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstadoCuentaCorriente.Location = new System.Drawing.Point(20, 297);
+            this.lblEstadoCuentaCorriente.Name = "lblEstadoCuentaCorriente";
+            this.lblEstadoCuentaCorriente.Size = new System.Drawing.Size(89, 23);
+            this.lblEstadoCuentaCorriente.TabIndex = 5;
+            this.lblEstadoCuentaCorriente.Text = "Resultado";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(776, 595);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(86, 34);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(665, 595);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(86, 34);
+            this.btnAceptar.TabIndex = 8;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // lvwEstadoCuentaCorriente
+            // 
+            this.lvwEstadoCuentaCorriente.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colPadding,
+            this.colNroFactura,
+            this.colFecha,
+            this.colImporte,
+            this.colEstado,
+            this.colAccion});
+            this.lvwEstadoCuentaCorriente.FullRowSelect = true;
+            this.lvwEstadoCuentaCorriente.GridLines = true;
+            this.lvwEstadoCuentaCorriente.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvwEstadoCuentaCorriente.HideSelection = false;
+            this.lvwEstadoCuentaCorriente.Location = new System.Drawing.Point(25, 324);
+            this.lvwEstadoCuentaCorriente.Name = "lvwEstadoCuentaCorriente";
+            this.lvwEstadoCuentaCorriente.Size = new System.Drawing.Size(839, 252);
+            this.lvwEstadoCuentaCorriente.TabIndex = 9;
+            this.lvwEstadoCuentaCorriente.TabStop = false;
+            this.lvwEstadoCuentaCorriente.UseCompatibleStateImageBehavior = false;
+            this.lvwEstadoCuentaCorriente.View = System.Windows.Forms.View.Details;
+            // 
+            // colPadding
+            // 
+            this.colPadding.DisplayIndex = 5;
+            this.colPadding.Text = "\"\"";
+            this.colPadding.Width = 1;
+            // 
+            // colNroFactura
+            // 
+            this.colNroFactura.DisplayIndex = 0;
+            this.colNroFactura.Text = "Nº Factura";
+            this.colNroFactura.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.colNroFactura.Width = 140;
+            // 
+            // colFecha
+            // 
+            this.colFecha.DisplayIndex = 1;
+            this.colFecha.Text = "Fecha";
+            this.colFecha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.colFecha.Width = 140;
+            // 
+            // colImporte
+            // 
+            this.colImporte.DisplayIndex = 2;
+            this.colImporte.Text = "Importe";
+            this.colImporte.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.colImporte.Width = 140;
+            // 
+            // colEstado
+            // 
+            this.colEstado.DisplayIndex = 3;
+            this.colEstado.Text = "Estado";
+            this.colEstado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.colEstado.Width = 275;
+            // 
+            // colAccion
+            // 
+            this.colAccion.DisplayIndex = 4;
+            this.colAccion.Text = "Acción";
+            this.colAccion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.colAccion.Width = 140;
+            // 
+            // FormEstadoCuentasCorrientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 645);
+            this.Controls.Add(this.lvwEstadoCuentaCorriente);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.lblEstadoCuentaCorriente);
+            this.Controls.Add(this.btnLimpiarFiltros);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.grpFiltros);
+            this.Controls.Add(this.grpCliente);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormEstadoCuentasCorrientes";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Estado de Cuentas Corrientes";
+            this.grpCliente.ResumeLayout(false);
+            this.grpCliente.PerformLayout();
+            this.grpFiltros.ResumeLayout(false);
+            this.grpFiltros.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.GroupBox grpCliente;
+        private System.Windows.Forms.Button btnBuscarCliente;
+        private System.Windows.Forms.TextBox txtCUIT;
+        private System.Windows.Forms.Label lblCUIT;
+        private System.Windows.Forms.GroupBox grpFiltros;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.TextBox txtNroFactura;
+        private System.Windows.Forms.Label lblNroFactura;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiarFiltros;
+        private System.Windows.Forms.Label lblEstadoCuentaCorriente;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ListView lvwEstadoCuentaCorriente;
+        private System.Windows.Forms.ColumnHeader colFecha;
+        private System.Windows.Forms.ColumnHeader colImporte;
+        private System.Windows.Forms.ColumnHeader colEstado;
+        private System.Windows.Forms.ColumnHeader colAccion;
+        private System.Windows.Forms.ColumnHeader colPadding;
+        private System.Windows.Forms.ColumnHeader colNroFactura;
+    }
+}
+
