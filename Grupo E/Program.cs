41ce030d85a5c3834db9406d75460c34a12f2ab6
lederkremermagali <lederkremermagali@gmail.com>
2025-10-07 14:01:17 -0300

    Sumamos en el program.CS para que se puedan correr los formularios

--- Grupo E/Program.cs
+++ Grupo E/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Grupo_E
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Grupo_E.RendicionHDRLargaDistancia.Form1());
+            //Application.Run(new Grupo_E.ConsultarEstadoEncomienda.EstadoDeEncomiedaForm());
+            //Application.Run(new Grupo_E.GeneracionDeFacturas.GeneracionDeFacturas());
+            //Application.Run(new Grupo_E.ImposicionEnAgencia.ImposicionEnAgencia());
+            //Application.Run(new Grupo_E.ConsultarEstadoEncomienda.EstadoDeEncomiedaForm());
+
+
+
         }
     }
 }
