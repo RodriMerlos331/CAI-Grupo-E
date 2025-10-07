c9d90482c5565d12796fb275fab652878b86245e
lederkremermagali <lederkremermagali@gmail.com>
2025-10-07 14:59:33 -0300

    Creo clases para GestionarFletero

---
+++ Grupo E/GestionarFletero/Guia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_E.GestionarFletero
+{
+    internal class Guia
+    {
+        public int HDR {  get; set; }
+        public int Tracking {  get; set; }
+
+        public string Estado { get; set; }
+    }
+}
