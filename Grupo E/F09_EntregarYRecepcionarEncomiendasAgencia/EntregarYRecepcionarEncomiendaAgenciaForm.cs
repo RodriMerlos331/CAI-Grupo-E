6f0813ee74c209e547cfb6a6bd65ced30e62b202
RodriMerlos331 <merlosrodrigo25@gmail.com>
2025-10-13 00:13:30 -0300

    Validaciones de Entrega y Recepcion en Agencia

--- Grupo E/F09_EntregarYRecepcionarEncomiendasAgencia/EntregarYRecepcionarEncomiendaAgenciaForm.cs
+++ Grupo E/F09_EntregarYRecepcionarEncomiendasAgencia/EntregarYRecepcionarEncomiendaAgenciaForm.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Grupo_E.F09_EntregarYRecepcionarEncomiendasAgencia;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,8 @@ namespace Grupo_E.EntregarYRecepcionarEncomiendaAgencia
 {
     public partial class EntregarYRecepcionarEncomiendaAgenciaForm : Form
     {
+
+        private readonly EntregaYRecepcionEncomiendaAgenciaModel modelo = new EntregaYRecepcionEncomiendaAgenciaModel();    
         public EntregarYRecepcionarEncomiendaAgenciaForm()
         {
             InitializeComponent();
@@ -20,6 +25,74 @@ namespace Grupo_E.EntregarYRecepcionarEncomiendaAgencia
         private void EntregarYRecepcionarEncomiendaAgenciaForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void BuscarBtn_Click(object sender, EventArgs e)
+        {
+            GuiasAEntregarListView.Items.Clear();
+            GuiasARecibirListView.Items.Clear();
+
+            if (string.IsNullOrWhiteSpace(DNITxt.Text))
+            {
+                MessageBox.Show("Ingresá un DNI.", "Atención",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                DNITxt.Focus();
+                return;
+            }
+
+            int dni;
+
+            if (!int.TryParse(DNITxt.Text.Trim(), out dni))
+            {
+                MessageBox.Show("Ingresá un DNI válido.", "Atención",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                DNITxt.Focus();
+                return;
+            }
+
+            foreach (KeyValuePair<int, GuiaDeEncomiendas> numGuia in modelo.data)
+            {
+                GuiaDeEncomiendas guia = numGuia.Value;
+
+                if (guia.FleteroAsignado.DNI == dni)
+                {
+                    if (guia.EstadoEnvio == EstadoDeEnvio.RuteadaRetiroAgencia)
+                    {
+                        GuiasAEntregarListView.Items.Add(
+                            new ListViewItem(guia.TrackingId.ToString()));
+                    }
+                    else if (guia.EstadoEnvio == EstadoDeEnvio.RuteadaEntregaAgencia)
+                    {
+                        GuiasARecibirListView.Items.Add(
+                            new ListViewItem(guia.TrackingId.ToString()));
+                    }
+                }
+            }
+        }
+
+        private void LimpiarBtn_Click(object sender, EventArgs e)
+        {
+            DNITxt.Clear();
+            GuiasAEntregarListView.Items.Clear();
+            GuiasARecibirListView.Items.Clear();
+            DNITxt.Focus();
+        }
+
+        private void AceptarBtn_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in GuiasAEntregarListView.Items)
+            {
+                int trackingId = int.Parse(item.Text);
+                modelo.data[trackingId].EstadoEnvio = EstadoDeEnvio.ImpuestaRetiradaAgencia;
+            }
+
+            foreach (ListViewItem item in GuiasARecibirListView.Items)
+            {
+
+                int trackingId = int.Parse(item.Text);
+                modelo.data[trackingId].EstadoEnvio = EstadoDeEnvio.PendienteRetiroAgencia;
+     
+            }
         }
     }
 }
---
+++ Grupo E/F09_EntregarYRecepcionarEncomiendasAgencia/GuiaDeEncomiendas.cs
@@ -0,0 +1,24 @@
+﻿
+using Grupo_E.EntregarYRecepcionarEncomiendaAgencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_E.F09_EntregarYRecepcionarEncomiendasAgencia
+{
+    internal class GuiaDeEncomiendas
+    {
+        public int TrackingId { get; set; }
+        public EstadoDeEnvio EstadoEnvio { get; set; }
+        public string CUIT { get; set; }
+        public string NombreDestinatario { get; set; }
+        public string ApellidoDestinatario { get; set; }
+        public int DNIDestinatario { get; set; }
+        public string LocalidadDestino { get; set; }
+        public string AgenciaDestino { get; set; }
+        public string TamañoBulto { get; set; }
+        public Fletero FleteroAsignado { get; set; }
+    }
+}
