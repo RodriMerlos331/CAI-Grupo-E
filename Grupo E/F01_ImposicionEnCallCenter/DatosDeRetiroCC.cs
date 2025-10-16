3292e03d71a94916eee03e8b6b59a4b3820d1958
Julieta Carullo <julietacarullo8@gmail.com>
2025-10-16 20:38:07 -0300

    F01 -  Datos de retiro

---
+++ Grupo E/F01_ImposicionEnCallCenter/DatosDeRetiroCC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_E.F01_ImposicionEnCallCenter
+{
+    internal class DatosDeRetiroCC
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Ciudad { get; set; }
+        public string Calle { get; set; }
+        public string Altura { get; set; }
+        public string PisoDepartamento { get; set; }
+        public string CodigoPostal { get; set; }
+    }
+}
