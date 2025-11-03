0b62380e32b49b7e6b2543cfd92a7c47272fcec4
ConradoIannicelli <conradoiannicelli@gmail.com>
2025-11-03 12:11:08 -0300

    cambios conrado(factura almacen entidad y datos)

---
+++ Grupo E/Almacenes/FacturaAlmacen.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_E.Almacenes
+{
+    static class FacturaAlmacen
+    {
+        public static List<FacturaEnt> Facturas { get; }
+
+        static FacturaAlmacen()
+        {
+            if (File.Exists("Facturas.json"))
+            {
+                var FacturasJson = File.ReadAllText("Facturas.json");
+                Facturas = JsonConvert.DeserializeObject<List<FacturaEnt>>(FacturasJson);
+            }
+        }
+
+        public static void Grabar()
+        {
+            var FacturasJson = JsonConvert.SerializeObject(Facturas);
+            File.WriteAllText("Facturas.json", FacturasJson);
+        }
+    }
+}
---
+++ Grupo E/Almacenes/FacturaEnt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo_E.Almacenes
+{
+    internal class FacturaEnt
+    {
+      public int NroFactura { get; set; }
+       public string CuitTutasa { get; set; }
+        public string CuitCliente { get; set; }
+        public DateTime FechaEmision { get; set; }
+
+        public List<int> EncomiendasIncluidas { get; set; }`
+        public decimal Subtotal { get; set; }
+
+        public decimal Iva { get; set; }
+
+        public decimal Total { get; set; }
+         
+        public string CodigoDeAutorizacionElectronica { get; set; }
+    }
+}
